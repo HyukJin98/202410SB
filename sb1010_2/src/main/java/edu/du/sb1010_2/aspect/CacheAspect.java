b2a7f14c69df8037727bfba9d655e90163e4e16e
HyukJin98 <dlgurwls4321@gmail.com>
2024-10-11 15:34:11 +0900

    1011 update

--- sb1010_2/src/main/java/edu/du/sb1010_2/aspect/CacheAspect.java
+++ sb1010_2/src/main/java/edu/du/sb1010_2/aspect/CacheAspect.java
@@ -21,12 +21,12 @@ public class CacheAspect {
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         Long num = (Long) joinPoint.getArgs()[0];
         if(cache.containsKey(num)) {
-            System.out.printf("CacheAspect: Cache에서 구함[%d]\n",num);
+            System.out.printf("CacheAspect: Cache에서 find[%d]\n",num);
             return cache.get(num);
         }
         Object result = joinPoint.proceed();
         cache.put(num, result);
-        System.out.printf("CacheAspect: Cache에 추가[%d]\n",num);
+        System.out.printf("CacheAspect: Cache에 append[%d]\n",num);
         return result;
     }
 }
